4b349e66758e2595b990c229eab336f38bd4482d
javainius <vainiusbaran@gmail.com>
2020-10-10 00:18:49 +0300

    videos parsed by their time spans

--- content.js
+++ content.js
@@ -1,15 +1,68 @@
 var vid =  document.querySelector('video');
-
-allVidDiv =  document.getElementById('items');
-
-vids = allVidDiv.children
+const desirableTimeSpan = 360; // seconds
 
 vid.addEventListener('ended', function(e) {
     var listOfVideos = getElementByXpath(
         '/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[4]/div[1]/div/div[11]/ytd-watch-next-secondary-results-renderer/div[2]')
-    console.log(listOfVideos);
+    // var achors = listOfVideos.getElementsByTagName('a');
+    // console.log(listOfVideos.childNodes);
+    // listOfVideos.childNodes.forEach(removeVideo)
+    getPlayableVideo(listOfVideos.childNodes);
+    // achors[0].click();
 });
 
 function getElementByXpath(path) {
     return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+}
+
+function getPlayableVideo(listOfVideos){
+    getRidOfUndesirableVideos(listOfVideos);
+}
+
+function getRidOfUndesirableVideos(listOfVideos){
+    var arrayOfVideos = Array.from(listOfVideos);
+    arrayOfVideos = arrayOfVideos.filter(video => !removeVideo(video));
+
+    console.log(arrayOfVideos);
+}
+
+function removeVideo(video){
+    if (isVideoTimeStatusSuitable(video)){
+        return false;
+    }
+    
+    return true;
+}
+
+function isVideoTimeStatusSuitable(video){
+    timeSpan = video.getElementsByClassName("ytd-thumbnail-overlay-time-status-renderer");
+    
+    if(timeSpan[1] !== undefined){
+        var timeStringParts = timeSpan[1].innerText.split(':');
+
+        if(timeStringParts.length > 2){
+            return false;
+        }
+
+        seconds = convertToSeconds(timeStringParts)
+
+        if(seconds <= desirableTimeSpan){
+            return true;
+        }
+        
+        return false;
+    }
+
+    return false;
+}
+
+function isVideoAlreadyWatched(video){
+    
+}
+
+function convertToSeconds(timeStringParts){
+    var minutes = parseInt(timeStringParts[0]);
+    var seconds = parseInt(timeStringParts[1]);
+
+    return minutes * 60 + seconds;
 }
