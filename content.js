1a83697a79ef863a4bf2c68850c3023e005436c1
javainius <vainiusbaran@gmail.com>
2020-10-09 21:56:46 +0300

    initial commit

---
+++ content.js
@@ -0,0 +1,17 @@
+
+
+var vid =  document.querySelector('video');
+
+allVidDiv =  document.getElementById('items');
+
+vids = allVidDiv.children
+
+vid.addEventListener('ended', function(e) {
+    var listOfVideos = getElementByXpath(
+        '/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[4]/div[1]/div/div[11]/ytd-watch-next-secondary-results-renderer/div[2]')
+    console.log(listOfVideos);
+});
+
+function getElementByXpath(path) {
+    return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+}
