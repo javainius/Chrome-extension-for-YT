a9c4127e3ed361efbd0aa781d9507a2935b62461
javainius <vainiusbaran@gmail.com>
2020-11-03 21:11:41 +0200

    UI works but need to fix one bug

--- content.js
+++ content.js
@@ -1,112 +1,121 @@
-chrome.runtime.onMessage.addListener(function (request, sender,
+chrome.runtime.onMessage.addListener(function (checkBoxBool, sender,
     sendResponse) {
-        console.log(sender);
-   sendResponse({count: matches.length, message: request})
+    if (checkBoxBool){
+        console.log("auto play is enabled")
+        enableAutoVideoPlay();
+    }
+    else{
+        console.log("auto play is disabled")
+    }
+    sendResponse({ message: "request"})
 })
 
-// var vid =  document.querySelector('video');
-// const desirableTimeSpan = 360; // seconds
-
-// vid.addEventListener('ended', function(e) {
-//     var listOfVideos = getElementByXpath(
-//         '/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[4]/div[1]/div/div[11]/ytd-watch-next-secondary-results-renderer/div[2]');
+const enableAutoVideoPlay = () => {
+    const vid =  document.querySelector('video');
+    const desirableTimeSpan = 360; // seconds
     
-//     if(listOfVideos === null){
-//         listOfVideos = getElementByXpath("/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[4]/div[2]/div/div[3]/ytd-watch-next-secondary-results-renderer/div[2]")
-//     }
-//     var nextVideo = getNextVideo(listOfVideos.childNodes);
-//     var nextVideoAchors = nextVideo.getElementsByTagName('a');
-
-//     nextVideoAchors[0].click();
-// });
-
-// function getElementByXpath(path) {
-//     return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-// }
-
-// function getNextVideo(listOfVideos){
-//     var arrayOfVideos = getRidOfUndesirableVideos(listOfVideos);
-
-//     return findVideoWithMostViews(arrayOfVideos);    
-// }
-
-// function getRidOfUndesirableVideos(listOfVideos){
-//     var arrayOfVideos = Array.from(listOfVideos);
-//     return arrayOfVideos.filter(video => !removeVideo(video));
-// }
-
-// function findVideoWithMostViews(arrayOfVideos){
-//     var mostViews = getNumberOfViews(arrayOfVideos[0]);
-//     var videoWithMostViews = arrayOfVideos[0];
-
-//     for(var video of arrayOfVideos){
-//         numberOfViews = getNumberOfViews(video);
-
-//         if(mostViews < numberOfViews){
-//             mostViews = numberOfViews;
-//             videoWithMostViews = video;
-//         }
-//     }
-
-//     return videoWithMostViews;
-// }
-
-// function removeVideo(video){
-
-//     if (isVideoTimeStatusSuitable(video) && !isVideoAlreadyWatched(video)){
-//         return false;
-//     }
-
-//     return true;
-// }
-
-// function isVideoTimeStatusSuitable(video){
-//     timeSpan = video.getElementsByClassName("ytd-thumbnail-overlay-time-status-renderer");
-    
-//     if(timeSpan[1] !== undefined){
-//         var timeStringParts = timeSpan[1].innerText.split(':');
-
-//         if(timeStringParts.length > 2){
-//             return false;
-//         }
-
-//         seconds = convertToSeconds(timeStringParts)
-
-//         if(seconds <= desirableTimeSpan){
-//             return true;
-//         }
+    vid.addEventListener('ended', function(e) {
+        console.log("asfasdfasdfasdfasdf")
+        let listOfVideos = getElementByXpath(
+            '/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[4]/div[1]/div/div[11]/ytd-watch-next-secondary-results-renderer/div[2]');
         
-//         return false;
-//     }
-
-//     return false;
-// }
-
-// function isVideoAlreadyWatched(video){
-//     var watchedIndicator = video.querySelector("#progress");
-
-//     if(watchedIndicator === null)
-//         return false;
-//     return true;
-// }
-
-// function convertToSeconds(timeStringParts){
-//     var minutes = parseInt(timeStringParts[0]);
-//     var seconds = parseInt(timeStringParts[1]);
-
-//     return minutes * 60 + seconds;
-// }
-
-// function getNumberOfViews(video){
-//     var views = video.querySelector("span.ytd-video-meta-block").innerText
-//     var numberOfViews = parseFloat(views.replace(',', '.')) * getMultiplier(views);
+        if(listOfVideos === null){
+            listOfVideos = getElementByXpath("/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[4]/div[2]/div/div[3]/ytd-watch-next-secondary-results-renderer/div[2]")
+        }
+        let nextVideo = getNextVideo(listOfVideos.childNodes);
+        let nextVideoAchors = nextVideo.getElementsByTagName('a');
     
-//     return numberOfViews;
-// }
-
-// function getMultiplier(views){
-//     if(views.search("mln") !== -1) return 1000000;
-//     if(views.search("tūkst") !== -1) return 1000;
-//     if(views.search("mlrd") !== -1) return 1000000000;
-//     return 1;
-// }
+        nextVideoAchors[0].click();
+    });
+    
+    function getElementByXpath(path) {
+        return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+    }
+    
+    function getNextVideo(listOfVideos){
+        let arrayOfVideos = getRidOfUndesirableVideos(listOfVideos);
+    
+        return findVideoWithMostViews(arrayOfVideos);    
+    }
+    
+    function getRidOfUndesirableVideos(listOfVideos){
+        let arrayOfVideos = Array.from(listOfVideos);
+        return arrayOfVideos.filter(video => !removeVideo(video));
+    }
+    
+    function findVideoWithMostViews(arrayOfVideos){
+        let mostViews = getNumberOfViews(arrayOfVideos[0]);
+        let videoWithMostViews = arrayOfVideos[0];
+    
+        for(let video of arrayOfVideos){
+            numberOfViews = getNumberOfViews(video);
+    
+            if(mostViews < numberOfViews){
+                mostViews = numberOfViews;
+                videoWithMostViews = video;
+            }
+        }
+    
+        return videoWithMostViews;
+    }
+    
+    function removeVideo(video){
+    
+        if (isVideoTimeStatusSuitable(video) && !isVideoAlreadyWatched(video)){
+            return false;
+        }
+    
+        return true;
+    }
+    
+    function isVideoTimeStatusSuitable(video){
+        timeSpan = video.getElementsByClassName("ytd-thumbnail-overlay-time-status-renderer");
+        
+        if(timeSpan[1] !== undefined){
+            let timeStringParts = timeSpan[1].innerText.split(':');
+    
+            if(timeStringParts.length > 2){
+                return false;
+            }
+    
+            seconds = convertToSeconds(timeStringParts)
+    
+            if(seconds <= desirableTimeSpan){
+                return true;
+            }
+            
+            return false;
+        }
+    
+        return false;
+    }
+    
+    function isVideoAlreadyWatched(video){
+        let watchedIndicator = video.querySelector("#progress");
+    
+        if(watchedIndicator === null)
+            return false;
+        return true;
+    }
+    
+    function convertToSeconds(timeStringParts){
+        let minutes = parseInt(timeStringParts[0]);
+        let seconds = parseInt(timeStringParts[1]);
+    
+        return minutes * 60 + seconds;
+    }
+    
+    function getNumberOfViews(video){
+        let views = video.querySelector("span.ytd-video-meta-block").innerText
+        let numberOfViews = parseFloat(views.replace(',', '.')) * getMultiplier(views);
+        
+        return numberOfViews;
+    }
+    
+    function getMultiplier(views){
+        if(views.search("mln") !== -1) return 1000000;
+        if(views.search("tūkst") !== -1) return 1000;
+        if(views.search("mlrd") !== -1) return 1000000000;
+        return 1;
+    }
+}
--- popup.js
+++ popup.js
@@ -5,26 +5,22 @@ document.addEventListener('DOMContentLoaded', function () {
     function onclick () {
         chrome.tabs.query({currentWindow: true, active: true},
         function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, 'hi', setCount)
+            let checkBox = document.querySelector('#validator');
+            chrome.tabs.sendMessage(tabs[0].id, checkBox.checked, setCount);
         })
     }
-
+    function setCount (res) {
+        console.log(res.message);
+    }
 }, false)
 
-// document.addEventListener('DOMContentLoaded', function () {
-//     document.querySelector('button').addEventListener('click', 
-//     onclick, false)
 
-//     function onclick () {
-//         chrome.tabs.query({currentWindow: true, active: true},
-//         function (tabs) {
-//             chrome.tabs.sendMessage(tabs[0].id, 'hi', setCount)
-//         })
-//     }
 
-//     function setCount (res) {
-//         const div = document.createElement('div')
-//         div.textContent = `${res.count} bears ${res.message}`
-//         document.body.appendChild(div)
-//     }
-// }, false)
+chrome.tabs.query({currentWindow: true, active: true},
+    function (tabs) {
+        let checkBox = document.querySelector('#validator');
+        chrome.tabs.sendMessage(tabs[0].id, checkBox.checked, setCount);
+    })
+function setCount (res) {
+    console.log(res.message);
+}
